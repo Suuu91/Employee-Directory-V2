525dd150998b792e9a310b90aee6dc31820b6c03
zhejun su <suzhejunk9@gmail.com>
2024-11-14 04:29:55 -0500

    add router and middleware

---
+++ api/employees-router.js
@@ -0,0 +1,32 @@
+const express = require(`express`)
+const router = express.Router()
+
+module.export = router
+
+const employee = require(`../employees.js`);
+const employees = require("../employees.js");
+
+router.get("/", (req, res) => {
+  res.json(employees);
+});
+
+router.get("/random", (req, res) => {
+  const i = Math.floor(Math.random() * employees.length);
+  res.json(employees[i]);
+});
+
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const employee = employees.find((e) => e.id === +id);
+  if (employee) {
+    res.json(employee);
+  } else {
+    next({status: 404, message: `no employee found with id ${id}`});
+  }
+});
+
+router.post("/", (req, res ,next) => {
+  const {newEmployee} = req.body
+  employees.push(newEmployee)
+  res.status(201).json(employees)
+})
--- server.js
+++ server.js
@@ -2,30 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
 const employees = require("./employees");
 
-app.get("/employees", (req, res) => {
-  res.json(employees);
-});
+app.use(`/employees`,require(`./api/employees-router.js`))
 
-app.get("/employees/random", (req, res) => {
-  const i = Math.floor(Math.random() * employees.length);
-  res.json(employees[i]);
-});
+app.use((req, res, next) => {
+  next({status:404, message: `we couldn't find the endpoint ${req.originalUrl}`})
+})
 
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
-  if (employee) {
-    res.json(employee);
-  } else {
-    res.status(404).send(`There is no employee with id ${id}.`);
-  }
-});
+app.use((err,req,res,next) => {
+  console.error(err)
+  res.send(err.message ?? `error`)
+  res.status(err.status ?? 500)
+})
 
 app.listen(PORT, () => {
   `Listening on port ${PORT}...`;
